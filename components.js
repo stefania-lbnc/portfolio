7f6c1221a7f8b884c7c381cd975859b220a46a14
StefaniaLoBianco <lobiancostefania@gmail.com>
2024-06-12 12:12:28 +0200

    toggle

--- components.js
+++ components.js
@@ -108,14 +108,14 @@ class mobileHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <nav id="mobile-header" class="top-bar-background mobile">
-            <div class="hamburger-menu">
+                <div class="hamburger-menu">
                 <div class="hamburger-icon" onClick="toggleMenu()">
                     <!-- these three span are going to be the lines of the hamburger menu-->
                     <span></span>
                     <span></span>
                     <span></span>
                 </div>
-                <div class="menu-links collapsible">
+                <div class="menu-links collapsible" style="background-color: white">
                     <ul>
                         <li>
                             <button class="menu-mobile menu-btn">
@@ -135,7 +135,36 @@ class mobileHeader extends HTMLElement {
                     </ul>
                 </div>
             </div>
-        </nav>
+            </nav>
+            <nav id="mobile-header" class="mobile">
+                <div class="hamburger-menu">
+                <div class="hamburger-icon" onClick="toggleMenu()">
+                    <!-- these three span are going to be the lines of the hamburger menu-->
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="menu-links collapsible" style="background-color: white">
+                    <ul>
+                        <li>
+                            <button class="menu-mobile menu-btn">
+                                <a href="index.html#home">home</a>
+                            </button>
+                        </li>
+                        <li>
+                            <button class="menu-mobile menu-btn">
+                                <a href="index.html#profile">profile</a>
+                            </button>
+                        </li>
+                        <li>
+                            <button class="menu-mobile menu-btn">
+                                <a href="index.html#projects">projects</a>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </nav>
         `;
     }
 }
